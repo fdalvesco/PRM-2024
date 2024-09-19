89e49233fd56b5fadc038e3cebcbe6dbb6922f88
Fernando Dal Vesco <101358513+fdalvesco@users.noreply.github.com>
2024-09-19 12:41:20 -0300

    Aula 12/09/2024

--- mater-play-frontend/src/app/components/MovieCard/index.tsx
+++ mater-play-frontend/src/app/components/MovieCard/index.tsx
@@ -1,7 +1,27 @@
-function MovieCard(){
-    return (
-        <></>
-    )
+import { Paper } from "@mui/material";
+
+type MovieCardProps = {
+  poster?: string;
+}
+function MovieCard({
+  poster
+}: MovieCardProps) {
+  return (
+    <Paper
+      component="a"
+      elevation={0}
+      href="/1"
+      sx={{
+        minWidth: '10rem'
+      }}
+    >
+      <img src={ poster }
+        style={{
+          width: '100%'
+        }}
+      />
+    </Paper>
+  )
 }
 
-export default MovieCard
+export default MovieCard;
